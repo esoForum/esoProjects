a7263906972ab496bf0c8fb36291e0cea9f83f1f
irix <ir1x@protonmail.com>
2018-11-21 18:30:28 -0600

    Create searchResults.inc.php

---
+++ views/searchResults.inc.php
@@ -0,0 +1,28 @@
+<?php
+if(!defined("IN_ESOTALK"))exit;?>
+<table cellspacing='0' cellpadding='2' class='c'>
+<thead>
+<tr><?php foreach($this->resultsTable as $column):?><th<?php if(!empty($column["class"])):?> class='<?php echo $column["class"];?>'<?php endif;?>><?php echo !empty($column["title"])?$column["title"]:"&nbsp;";?></th><?php endforeach;?></tr>
+<tr id='newResults' style='display:none'><td colspan='<?php echo count($this->resultsTable);?>'><?php echo $this->esoTalk->htmlMessage("newSearchResults");?></td></tr>
+</thead>
+<tbody id='conversations'>
+
+<?php
+function columnStar(&$search,$conversation){return $search->esoTalk->htmlStar($conversation["id"],$conversation["starred"]);}function columnAvatar(&$search,$conversation){return "<img src='".$search->esoTalk->getAvatar($conversation["startMemberId"],$conversation["avatarFormat"],"thumb")."' alt='' class='thumb'/>";}function columnConversation(&$search,$conversation){global $language;$labels=explode(",",$conversation["labels"]);$i=0;$labelsHtml="";$html="";foreach($search->esoTalk->labels as $k=>$v){if(@$labels[$i])$labelsHtml.="<span class='label $k'>{$language["labels"][$k]}</span> ";$i++;}if($labelsHtml)$html.="<span class='labels'>$labelsHtml</span>";$html.="<strong";if($search->esoTalk->user and !$conversation["unread"])$html.=" class='read'";$html.="><a href='".makeLink($conversation["id"],$conversation["slug"])."' data-instant>".highlight($conversation["title"],$_SESSION["highlight"])."</a></strong>";if($search->esoTalk->user["name"] and $conversation["unread"])$html.="<small><a href='".makeLink($conversation["id"],$conversation["slug"],"?start=unread")."'>{$language["Jump to unread"]}</a></small>";else $html.="<small><a href='".makeLink($conversation["id"],$conversation["slug"],"?start=last")."'>{$language["Jump to last"]}</a></small>";$html.="<br/><small class='tags'>{$conversation["tags"]}</small>";$search->callHook("getConversationColumn",array(&$html,$conversation));return $html;}function columnPosts(&$search,$conversation){return "<span class='postCount p".(($conversation["posts"]>50)?"1":(($conversation["posts"]>10)?"2":"3"))."'>{$conversation["posts"]}</span>";}function columnAuthor(&$search,$conversation){return "<a href='".makeLink("profile",$conversation["startMemberId"])." '>{$conversation["startMember"]}</a><br/><small>".relativeTime($conversation["startTime"])."</small>";}function columnLastReply(&$search,$conversation){$html="<span class='lastPostMember'>";if($conversation["posts"]>1)$html.="<a href='".makeLink("profile",$conversation["lastPostMemberId"])."'>{$conversation["lastPostMember"]}</a>";$html.="</span><br/><small class='lastPostTime'>";if($conversation["posts"]>1)$html.=relativeTime($conversation["lastPostTime"]);$html.="</small>";return $html;}if(count($this->results)):foreach($this->results as $conversation):?>
+
+<tr id='c<?php echo $conversation["id"];?>'<?php if($conversation["starred"]):?> class='starred'<?php endif;?>>
+<?php
+foreach($this->resultsTable as $column):?><td<?php if(!empty($column["class"])):?> class='<?php echo $column["class"];?>'<?php endif;?>><?php echo call_user_func_array($column["content"],array(&$this,$conversation));?></td>
+<?php endforeach;?>
+</tr>
+<?php endforeach;endif;?>
+
+</tbody>
+</table>
+
+<?php
+if(!$this->numberOfConversations):echo $this->esoTalk->htmlMessage("noSearchResults");elseif($this->limit==$config["results"]+1 and $this->numberOfConversations>$config["results"]):?>
+<div id='more'>
+<?php echo $this->esoTalk->htmlMessage("viewMore",array(makeLink("search",urlencode(@$_SESSION["search"].(@$_SESSION["search"]?" + ":"")."more results"))));?>
+</div>
+<?php endif;?>
