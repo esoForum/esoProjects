75ef2b7b6fcd844ee562362d82e6b4492fc7323c
irix <ir1x@protonmail.com>
2018-11-21 18:36:53 -0600

    Create wrapper.php

---
+++ views/wrapper.php
@@ -0,0 +1,54 @@
+<?php
+if(!defined("IN_ESOTALK"))exit;?>
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=<?php echo $language["charset"];?>'/>
+<title><?php echo ($this->controller->title?$this->controller->title." - ":"").$config["forumTitle"];?></title>
+<?php echo $this->head();?> 
+</head>
+
+<body>
+<?php $this->callHook("pageStart");?>
+
+<div id='loading' style='display:none'><?php echo $language["Loading"];?></div>
+
+<?php echo $this->getMessages();?>
+
+<div id='wrapper'<?php if($this->action!="search"):?> class='small'<?php endif;?>>
+
+<div id='hdrbarpack'>
+
+<div id='hdr'>
+<h1><a href='' title='<?php echo $config["forumTitle"];?>'><img src='<?php echo !empty($config["forumLogo"])?$config["forumLogo"]:"skins/{$config["skin"]}/logo.svg";?>' alt=''/> <span id='forumTitle'><?php echo $config["forumTitle"];?></span></a></h1>
+<?php if($this->action=="search"):?>
+<p id='stats'>
+<?php foreach($this->getStatistics() as $k=>$v):?>
+<span id='statistic-<?php echo $k;?>'><?php echo $v;?></span><br/>
+<?php endforeach;?>
+</p>
+<?php else:include "views/searchForm.inc.php";endif;?>
+</div>
+	
+<div id='bar'>
+<?php if(count($this->bar["left"])):?><ul class='fl'><?php ksort($this->bar["left"]);foreach($this->bar["left"] as $v)echo "<li>$v</li>";?></ul><?php endif;?>
+<?php if(count($this->bar["right"])):?><ul class='fr'><?php ksort($this->bar["right"]);foreach($this->bar["right"] as $v)echo "<li>$v</li>";?></ul><?php endif;?>
+</div>
+
+</div>
+
+<div id='body'>
+<?php $this->controller->render();?>
+</div>
+
+<?php $this->callHook("footer");?>
+<div id='ftr'>
+<?php if(count($this->footer)):?><ul><?php
+ksort($this->footer);foreach($this->footer as $v)echo "<li>$v</li>";?></ul><?php endif;?>
+</div>
+
+</div>
+
+<?php $this->callHook("pageEnd");?>
+</body>
+</html>
