8498a817447d922c9555787dd226d81ae0d8dc9f
Inpace <13023526+inpaceus@users.noreply.github.com>
2020-01-30 16:42:13 -0600

    Update online.controller.php

--- controllers/online.controller.php
+++ controllers/online.controller.php
@@ -1,2 +1,29 @@
 <?php
-if(!defined("IN_ESOTALK"))exit;class online extends Controller{var $view="online.view.php";function init(){global $language,$config;$this->title=$language["Online members"];$this->esoTalk->addToHead("<meta name='robots' content='noindex, noarchive'/>");$this->online=$this->esoTalk->db->query("SELECT memberId, name, avatarFormat, IF(color>{$this->esoTalk->skin->numberOfColors},{$this->esoTalk->skin->numberOfColors},color), account, lastSeen, lastAction FROM {$config["tablePrefix"]}members WHERE UNIX_TIMESTAMP()-{$config["userOnlineExpire"]}<lastSeen ORDER BY lastSeen DESC");$this->numberOnline=$this->esoTalk->db->numRows($this->online);}}?>
+// This file is part of esoForum for Projects.
+// All non-modified code is property of Simon and Toby Zerner.
+
+// Online controller: fetches a list of members currently online, ready to be displayed in the view.
+
+if (!defined("IN_ESOTALK")) exit;
+
+class online extends Controller {
+	
+var $view = "online.view.php";
+
+function init()
+{
+	global $language, $config;
+	
+	// Set the title and make sure this page isn't indexed.
+	$this->title = $language["Online members"];
+	$this->esoTalk->addToHead("<meta name='robots' content='noindex, noarchive'/>");
+	
+	// Fetch a list of members who have been logged in the members table as 'online' in the last
+	// $config["userOnlineExpire"] seconds.
+	$this->online = $this->esoTalk->db->query("SELECT memberId, name, avatarFormat, IF(color>{$this->esoTalk->skin->numberOfColors},{$this->esoTalk->skin->numberOfColors},color), account, lastSeen, lastAction FROM {$config["tablePrefix"]}members WHERE UNIX_TIMESTAMP()-{$config["userOnlineExpire"]}<lastSeen ORDER BY lastSeen DESC");
+	$this->numberOnline = $this->esoTalk->db->numRows($this->online);
+}
+	
+}
+
+?>
