dd55ef77f576fe5a5335587a2e54113e24046c86
Inpace <13023526+inpaceus@users.noreply.github.com>
2020-01-30 13:31:10 -0600

    Update forgot-password.controller.php

--- controllers/forgot-password.controller.php
+++ controllers/forgot-password.controller.php
@@ -1,2 +1,80 @@
 <?php
-if(!defined("IN_ESOTALK"))exit;class forgotpassword extends Controller{var $view="forgotPassword.view.php";var $title="";var $errors=array();var $setPassword=false;function init(){global $language,$messages,$config;if($this->esoTalk->user)redirect("");$this->title=$language["Forgot your password"];if($hash=@$_GET["q2"]){$result=$this->esoTalk->db->query("SELECT memberId FROM {$config["tablePrefix"]}members WHERE resetPassword='$hash'");if(!$this->esoTalk->db->numRows($result))redirect("forgotPassword");list($memberId)=$this->esoTalk->db->fetchRow($result);$this->setPassword=true;if(isset($_POST["changePassword"])){$password=@$_POST["password"];$confirm=@$_POST["confirm"];if($error=validatePassword(@$_POST["password"]))$this->errors["password"]=$error;if($password!=$confirm)$this->errors["confirm"]="passwordsDontMatch";if(!count($this->errors)){$passwordHash=md5($config["salt"].$password);$this->esoTalk->db->query("UPDATE {$config["tablePrefix"]}members SET resetPassword=NULL, password='$passwordHash' WHERE memberId=$memberId");$this->esoTalk->message("passwordChanged",false);redirect("");}}}if(isset($_POST["email"])){$result=$this->esoTalk->db->query("SELECT memberId, name, email FROM {$config["tablePrefix"]}members WHERE email='{$_POST["email"]}'");if(!$this->esoTalk->db->numRows($result)){$this->esoTalk->message("emailDoesntExist");return;}list($memberId,$name,$email)=$this->esoTalk->db->fetchRow($result);$hash=md5(rand());$this->esoTalk->db->query("UPDATE {$config["tablePrefix"]}members SET resetPassword='$hash' WHERE memberId=$memberId");if(sendEmail($email,sprintf($language["emails"]["forgotPassword"]["subject"],$name),sprintf($language["emails"]["forgotPassword"]["body"],$name,$config["forumTitle"],$config["baseURL"].makeLink("forgot-password",$hash)))){$this->esoTalk->message("passwordEmailSent",false);redirect("");}}}}
+// This file is part of esoForum for Projects.
+// All non-modified code is property of Simon and Toby Zerner.
+
+// Forgot password controller: sends a user an email containing a link to reset their password, and handles this
+// link to enable the user to set a new password.
+
+if (!defined("IN_ESOTALK")) exit;
+
+class forgotpassword extends Controller {
+
+var $view = "forgotPassword.view.php";
+var $title = "";
+var $errors = array();
+var $setPassword = false;
+
+function init()
+{
+	global $language, $messages, $config;
+
+	// If the user is logged in, kick them out.
+	if ($this->esoTalk->user) redirect("");
+	
+	// Set the title.
+	$this->title = $language["Forgot your password"];
+	
+	// If a password reset token has been provided, ie. they've clicked the link in their email...
+	if ($hash = @$_GET["q2"]) {
+		
+		// Find the user with this password reset token. If it's an invalid token, take them back to the email form.
+		$result = $this->esoTalk->db->query("SELECT memberId FROM {$config["tablePrefix"]}members WHERE resetPassword='$hash'");
+		if (!$this->esoTalk->db->numRows($result)) redirect("forgotPassword");
+		list($memberId) = $this->esoTalk->db->fetchRow($result);
+		
+		$this->setPassword = true;
+		
+		// If the change password form has been submitted...
+		if (isset($_POST["changePassword"])) {
+			
+			// Validate the passwords they entered.
+			$password = @$_POST["password"];
+			$confirm = @$_POST["confirm"];
+			if ($error = validatePassword(@$_POST["password"])) $this->errors["password"] = $error;
+			if ($password != $confirm) $this->errors["confirm"] = "passwordsDontMatch";
+			
+			// If it's all good, update the password in the database, show a success message, and redirect.
+			if (!count($this->errors)) {
+				$passwordHash = md5($config["salt"] . $password);
+				$this->esoTalk->db->query("UPDATE {$config["tablePrefix"]}members SET resetPassword=NULL, password='$passwordHash' WHERE memberId=$memberId");
+				$this->esoTalk->message("passwordChanged", false);
+				redirect("");
+			}
+		}
+	}
+	
+	// If they've submitted their email to get a password reset link, email one to them!
+	if (isset($_POST["email"])) {
+		
+		// Find the member with this email.
+		$result = $this->esoTalk->db->query("SELECT memberId, name, email FROM {$config["tablePrefix"]}members WHERE email='{$_POST["email"]}'");
+		if (!$this->esoTalk->db->numRows($result)) {
+			$this->esoTalk->message("emailDoesntExist");
+			return;
+		}
+		list($memberId, $name, $email) = $this->esoTalk->db->fetchRow($result);
+		
+		// Update their record in the database with a special password reset hash.
+		$hash = md5(rand());
+		$this->esoTalk->db->query("UPDATE {$config["tablePrefix"]}members SET resetPassword='$hash' WHERE memberId=$memberId");
+		
+		// Send them email containing the link, and redirect to the home page.
+		if (sendEmail($email, sprintf($language["emails"]["forgotPassword"]["subject"], $name), sprintf($language["emails"]["forgotPassword"]["body"], $name, $config["forumTitle"], $config["baseURL"] . makeLink("forgot-password", $hash)))) {
+			$this->esoTalk->message("passwordEmailSent", false);
+			redirect("");
+		}
+	}
+}
+
+
+}
