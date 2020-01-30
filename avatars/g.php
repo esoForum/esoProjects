a5c50f772f7fae064ea27898a39a92c019fa7909
Inpace <13023526+inpaceus@users.noreply.github.com>
2020-01-30 13:13:29 -0600

    Update g.php

--- avatars/g.php
+++ avatars/g.php
@@ -1,2 +1,24 @@
 <?php
-if(!$memberId=(int)@$_GET["id"])exit;$filename="$memberId.gif";if(!file_exists($filename))exit;header("Content-Type: image/gif");header("Content-Description: File Transfer");header("Content-Disposition: attachment; filename=\"$filename\"");header("Content-Transfer-Encoding: binary");header("Expires: 0");header("Cache-Control: must-revalidate, post-check=0, pre-check=0");header("Pragma: public");header("Content-Length: ".filesize($filename));set_time_limit(0);ob_clean();flush();readfile($filename);exit;?>
+// This file is part of esoForum for Projects.
+// All non-modified code is property of Simon and Toby Zerner.
+
+// GIF avatar loader: displays an unresized gif avatar with secure headers.
+
+if (!$memberId = (int)@$_GET["id"]) exit;
+$filename = "$memberId.gif";
+if (!file_exists($filename)) exit;
+header("Content-Type: image/gif");
+header("Content-Description: File Transfer");
+header("Content-Disposition: attachment; filename=\"$filename\""); // Filename.
+header("Content-Transfer-Encoding: binary");
+header("Expires: 0");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Pragma: public");
+header("Content-Length: " . filesize($filename));
+set_time_limit(0);
+ob_clean();
+flush();
+readfile($filename);
+exit;
+
+?>
